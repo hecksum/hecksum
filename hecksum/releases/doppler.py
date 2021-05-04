900155f24a8c5ad43d885a2e8dfa35a346731180
Miguel Rios <mrios0495@gmail.com>
2021-05-04 19:48:37 -0400

    Initial commit of releases module and doppler releases.

---
+++ hecksum/releases/doppler.py
@@ -0,0 +1,15 @@
+doppler_releases = [
+    { "os": "linux", "architecture": " amd64" }, { "os": "linux", "architecture": " i386" },
+    { "os": "linux", "architecture": " arm64" }, { "os": "linux", "architecture": " armv6" },
+    { "os": "linux", "architecture": " armv7" }, { "os": "openbsd", "architecture": " amd64" },
+    { "os": "openbsd", "architecture": " i386" }, { "os": "openbsd", "architecture": " arm64" },
+    { "os": "openbsd", "architecture": " armv6" }, { "os": "openbsd", "architecture": " armv7" },
+    { "os": "freebsd", "architecture": " amd64" }, { "os": "freebsd", "architecture": " arm64" },
+    { "os": "freebsd", "architecture": " armv6" }, { "os": "freebsd", "architecture": " armv7" },
+    { "os": "netbsd", "architecture": " amd64" }, { "os": "netbsd", "architecture": " i386" },
+    { "os": "netbsd", "architecture": " armv6" }, { "os": "netbsd", "architecture": " armv7" },
+    { "os": "macOS", "architecture": " amd64" }, { "os": "macOS", "architecture": " arm64" },
+    { "os": "windows", "architecture": " amd64" }, { "os": "windows", "architecture": " armv6" },
+    { "os": "windows", "architecture": " armv7" },
+]
+
